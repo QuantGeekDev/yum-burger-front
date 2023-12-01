1e7d5a4228a8737ef1f69e3953780feb72240920
Alex Andru <alex007d@gmail.com>
2023-12-01 15:31:21 +0100

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header = (): React.ReactElement => (
+  <header className="header">
+    <img
+      className="header__logo"
+      alt="Nyam Burger Logo"
+      height="57"
+      width="57"
+    />
+
+    <h1 className="header__title">NyamBurger</h1>
+  </header>
+);
+
+export default Header;

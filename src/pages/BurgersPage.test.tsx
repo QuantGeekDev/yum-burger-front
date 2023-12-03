9a68fbe4131aeb2aa1a9479ab78f8fc37f5acf7a
Alex Andru <alex007d@gmail.com>
2023-12-03 16:35:35 +0100

    Add BurgersPage tests

---
+++ src/pages/BurgersPage.test.tsx
@@ -0,0 +1,16 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../utils/renderWithProviders";
+import BurgersPage from "./BurgersPage";
+
+describe("Given a BurgersPage component", () => {
+  describe("When it is rendered", () => {
+    test("Then the title 'Our Burgers' should be visible", () => {
+      const titleText = "Our Burgers";
+
+      renderWithProviders(<BurgersPage />);
+      const actualBurgerPage = screen.getByRole("heading", { name: titleText });
+
+      expect(actualBurgerPage).toBeVisible();
+    });
+  });
+});

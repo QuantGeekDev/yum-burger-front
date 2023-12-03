4688f668b0dbfe440c8a489f7d8857c2bfb6ad22
Alex Andru <alex007d@gmail.com>
2023-12-03 16:44:34 +0100

    Revert "Merge branch 'feature/burgersPage' of https://github.com/isdi-coders-2023/Alex-Andrushevich-Final-Project-front-202309-bcn into feature/burgersPage"
    This reverts commit cf20971295174d48ed66d9d4de8e1794e31eb871, reversing
    changes made to f09099b038ab6cf8bef150f9be09da5cd04e5f09.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,12 @@
-import BurgersPage from "../../pages/BurgersPage";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
-import AppStyled from "./AppStyled";
 
 const App = (): React.ReactElement => (
-  <AppStyled>
+  <>
+    {" "}
     <Header />
     <NavigationBar />
-    <BurgersPage />
-  </AppStyled>
+  </>
 );
 
 export default App;
--- src/components/App/AppStyled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const AppStyled = styled.main`
-  display: flex;
-  flex-direction: column;
-`;
-
-export default AppStyled;

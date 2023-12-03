83f017f029274b3c8eb2ed4d4cb8d87fc28708f2
Alex Andru <alex007d@gmail.com>
2023-12-04 00:11:41 +0100

    Add Router to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
+import BurgersPage from "../../pages/BurgersPage";
 
-const App = (): React.ReactElement => (
-  <>
-    {" "}
-    <Header />
-    <NavigationBar />
-  </>
-);
+const App = (): React.ReactElement => {
+  return (
+    <>
+      <Header />
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Navigate to={"/home"} />} />
+        <Route path="/home" element={<BurgersPage />} />{" "}
+        <Route path="*" element={<h1> 404 Page Not Found </h1>} />{" "}
+      </Routes>
+    </>
+  );
+};
 
 export default App;

02d553c5fd24fd7b78cc5d50d1360ffea2155d38
Alex Andru <alex007d@gmail.com>
2023-12-03 16:34:24 +0100

    Add AppStyled

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
+import BurgersPage from "../../pages/BurgersPage";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
+import AppStyled from "./AppStyled";
 
 const App = (): React.ReactElement => (
-  <>
-    {" "}
+  <AppStyled>
     <Header />
     <NavigationBar />
-  </>
+    <BurgersPage />
+  </AppStyled>
 );
 
 export default App;
---
+++ src/components/App/AppStyled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const AppStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default AppStyled;

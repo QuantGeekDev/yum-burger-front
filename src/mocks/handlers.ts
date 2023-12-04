fbcc25413d514d119ee2d40fe500c1be7a228bf5
Alex Andru <alex007d@gmail.com>
2023-12-05 00:29:06 +0100

    Add MSW handlers

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,8 @@
+import { http, HttpResponse } from "msw";
+import { burgersMock } from "../store/features/burgers/mocks/burgersMock";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const handlers = [
+  http.get(`${apiUrl}/burgers`, () => HttpResponse.json(burgersMock)),
+];
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,10 @@
 import "@testing-library/jest-dom";
+import { server } from "./mocks/node";
 
 beforeEach(() => {
   vitest.clearAllMocks();
 });
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());

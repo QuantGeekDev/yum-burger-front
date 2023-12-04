63ace255082daff73a75fb84ac37585c505b4c25
Alex Andru <alex007d@gmail.com>
2023-12-04 13:02:09 +0100

    Add loadItems dispatch to BurgersPage

--- src/pages/BurgersPage.tsx
+++ src/pages/BurgersPage.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
 import BurgersPageStyled from "./BurgersPageStyled";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { loadBurgersActionCreator } from "../store/features/burgers/burgersSlice";
+import { burgersMock } from "../store/features/burgers/mocks/burgersMock";
 
-const BurgersPage = (): React.ReactElement => (
-  <BurgersPageStyled className="burgers-page">
-    <h2 className="burgers-page__title">Our Burgers</h2>
-  </BurgersPageStyled>
-);
+const BurgersPage = (): React.ReactElement => {
+  const burgers = useAppSelector((state) => state.rootReducer.burgers);
+  const dispatch = useAppDispatch();
+  const loadBurgersAction = loadBurgersActionCreator(burgersMock);
+
+  useEffect(() => {
+    dispatch(loadBurgersAction);
+  }, [dispatch, loadBurgersAction, burgers]);
+
+  return (
+    <BurgersPageStyled className="burgers-page">
+      <h2 className="burgers-page__title">Our Burgers</h2>
+    </BurgersPageStyled>
+  );
+};
 
 export default BurgersPage;

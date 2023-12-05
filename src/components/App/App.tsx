ed4eef2db598c4ba403291b7aaf24a575066b4e3
Alex Andru <alex007d@gmail.com>
2023-12-05 15:52:44 +0100

    Add padding to BurgerPage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,11 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
 import BurgersPage from "../../pages/BurgersPage";
+import AppStyled from "./AppStyled";
 
 const App = (): React.ReactElement => {
   return (
-    <>
+    <AppStyled>
       <Header />
       <NavigationBar />
       <Routes>
@@ -13,7 +14,7 @@ const App = (): React.ReactElement => {
         <Route path="/home" element={<BurgersPage />} />
         <Route path="*" element={<h1> 404 Page Not Found </h1>} />
       </Routes>
-    </>
+    </AppStyled>
   );
 };
 

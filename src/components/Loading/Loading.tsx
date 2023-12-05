83dad1c14767399d53a0f0d4acdfbdbe03e843ae
Alex Andru <alex007d@gmail.com>
2023-12-05 15:46:01 +0100

    Add Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,11 +1,21 @@
-const Loading = (): React.ReactElement => {
-  <div className="loading">
-    <video className="loading__spinner">
-      <source></source>
-      <track> </track>
+import LoadingStyled from "./LoadingStyled";
+
+const Loading = (): React.ReactElement => (
+  <LoadingStyled className="loading">
+    <video
+      className="loading__spinner"
+      autoPlay
+      loop
+      muted
+      playsInline
+      height="150"
+      width="150"
+    >
+      <source src="videos/burgerLoading.webm" type="video/webm" />
+      <track />
     </video>
-    <h1 className="loading__message"> Loading ...</h1>
-  </div>;
-};
+    <span className="loading__message"> Loading ...</span>
+  </LoadingStyled>
+);
 
 export default Loading;
--- src/components/Loading/LoadingStyled.ts
+++ src/components/Loading/LoadingStyled.ts
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
 const LoadingStyled = styled.div`
-  background-color: red;
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  background-color: #fffa;
+
+  .loading {
+    &__message {
+      text-align: center;
+      font-family: ${({ theme }) => theme.fonts.title};
+      font-size: 2rem;
+      color: ${({ theme }) => theme.colors.primary};
+    }
+  }
 `;
 
 export default LoadingStyled;

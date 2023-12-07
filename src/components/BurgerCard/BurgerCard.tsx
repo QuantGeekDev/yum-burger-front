4eed99b807cbb208d5f0ed3d39143117bad2dcd0
Alex Andru <alex007d@gmail.com>
2023-12-07 12:48:09 +0100

    Add 'Delete' button to BurgerCard

--- src/components/BurgerCard/BurgerCard.tsx
+++ src/components/BurgerCard/BurgerCard.tsx
@@ -1,12 +1,20 @@
 import { BurgerStructure } from "../../store/features/burgers/types";
 import BurgerCardStyled from "./BurgerCardStyled";
 import Button from "../Button/Button";
+import { useAppDispatch } from "../../store/hooks";
+import { showLoadingActionCreator } from "../../store/features/ui/uiSlice";
+import { toast } from "react-toastify";
+import { deleteBurgerActionCreator } from "../../store/features/burgers/burgersSlice";
 interface BurgerCardProps {
   burger: BurgerStructure;
 }
 const BurgerCard = ({
-  burger: { name, imageUrl },
+  burger: { name, imageUrl, _id },
 }: BurgerCardProps): React.ReactElement => {
+  const dispatch = useAppDispatch();
+  const deleteBurgerAction = deleteBurgerActionCreator(_id);
+  const dispatchDelete = () => dispatch(deleteBurgerAction);
+
   return (
     <BurgerCardStyled className="burger">
       <h2 className="burger__title">{name}</h2>
@@ -21,7 +29,12 @@ const BurgerCard = ({
         className="burger__delete-button button--transparent"
         text="Delete"
         actionOnClick={() => {
-          console.log("Hello world!");
+          try {
+            dispatchDelete();
+            toast.success("Burger deleted succesfully!");
+          } catch (error) {
+            toast.error("Error deleting burger, please try again later");
+          }
         }}
       />
     </BurgerCardStyled>

1be76c5c23ae8203a0358881f3593e6578f3c65f
Alex Andru <alex007d@gmail.com>
2023-12-08 20:11:08 +0100

    Add smartRenderWithProviders testing utility

--- src/testUtils/renderWithProviders.tsx
+++ src/testUtils/renderWithProviders.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components";
-import { RenderResult, render } from "@testing-library/react";
+import { RenderResult, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import defaultTheme from "../styles/DefaultTheme";
@@ -31,5 +31,36 @@ const renderWithProviders = (
       </BrowserRouter>
     </Provider>,
   );
+};
+
+export const smartRenderWithProviders = (
+  child: JSX.Element,
+  mockStore?: Store,
+) => {
+  const smartRender = () =>
+    render(
+      <Provider store={mockStore ?? store}>
+        <BrowserRouter>
+          <ThemeProvider theme={defaultTheme}>
+            <GlobalStyles />
+            <ToastContainer />
+            {child}
+          </ThemeProvider>
+        </BrowserRouter>
+      </Provider>,
+    );
+
+  const smartRenderWithProviderResponse = {
+    getByText: (text: string) => {
+      smartRender();
+      return screen.getByText(text);
+    },
+    getByRole: (role: string, options: { name: string }) => {
+      smartRender();
+      return screen.getByRole(role, options);
+    },
+  };
+
+  return smartRenderWithProviderResponse;
 };
 export default renderWithProviders;
